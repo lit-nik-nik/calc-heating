3626e7418746d505c1870fea6985ab2392e29d45
Nikolay <11arhangel05@mail.ru>
2020-08-14 23:33:21 +0300

    add id, label and function cleraForm

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 $(document).ready( function(){
 
-    $('.window').val('');
-
     let sum, 
         area,
         window,
@@ -16,8 +14,10 @@ $(document).ready( function(){
 
     $('button').on('click', () => {
         $('.calculator').toggleClass("hide show");
+        $('button').toggleClass('btn-danger btn-success');
         if ($('button').text() == 'Включить калькулятор') {
             $('button').text('Выключить калькулятор');
+            clearForm();
         } else {
             $('button').text('Включить калькулятор');
         }
@@ -25,57 +25,62 @@ $(document).ready( function(){
     });
 
     summa();
+    clearForm();
 
-    $('.area').on('input', () => {
-        area = +$('.area').val();
-        console.log(`area = ${area}`);
+    $('#area').on('input', () => {
+        area = +$('#area').val();
         summa();
     });
 
-    $('.window').on('change', () => {
-        window = +$('.window').val();
-        console.log(`window = ${window}`);
+    $('#window').on('change', () => {
+        window = +$('#window').val();
         summa();
     });
     
-    $('.thermal').on('change', () => {
-        thermal = +$('.thermal').val();
-        console.log(`thermal = ${thermal}`);
+    $('#thermal').on('change', () => {
+        thermal = +$('#thermal').val();
         summa();
     });
 
-    $('.area-window').on('change', () => {
-        areaWindow = +$('.area-window').val();
-        console.log(`areaWindow = ${areaWindow}`);
+    $('#area-window').on('change', () => {
+        areaWindow = +$('#area-window').val();
         summa();
     });
 
-    $('.temp').on('change', () => {
-        temp = +$('.temp').val();
-        console.log(`temp = ${temp}`);
+    $('#temp').on('change', () => {
+        temp = +$('#temp').val();
         summa();
     });
 
-    $('.wall').on('change', () => {
-        wall = +$('.wall').val();
-        console.log(`wall = ${wall}`);
+    $('#wall').on('change', () => {
+        wall = +$('#wall').val();
         summa();
     });
 
-    $('.top-room').on('change', () => {
-        topRoom = +$('.top-room').val();
-        console.log(`topRoom = ${topRoom}`);
+    $('#top-room').on('change', () => {
+        topRoom = +$('#top-room').val();
         summa();
     });
 
-    $('.height').on('change', () => {
-        height = +$('.height').val();
-        console.log(`height = ${height}`);
+    $('#height').on('change', () => {
+        height = +$('#height').val();
         summa();
     });
 
     function summa() {
         sum = Math.round(100 * area * window * thermal * areaWindow * temp * wall * topRoom * height); 
-        $('.sum').val(sum);
+        $('#sum').val(sum);
+    }
+
+    function clearForm() {
+        $('#area').val('');
+        $('#window').val('');
+        $('#thermal').val('');
+        $('#area-window').val('');
+        $('#temp').val('');
+        $('#wall').val('');
+        $('#top-room').val('');
+        $('#height').val('');
+        $('#sum').val(0);
     }
 });
